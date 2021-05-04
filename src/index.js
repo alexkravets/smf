8c4e9aeb82640c3e4ac1ae18d31f61360b8ce057
Oleksandr Kravets <a@kra.vc>
2021-05-04 18:15:41 +0300

    Fix exports

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const Youtube   = require('./Youtube')
 const Instagram = require('./Instagram')
 
 module.exports = {
-  twitter:   Twitter,
-  youtube:   Youtube,
-  instagram: Instagram
+  Twitter,
+  Youtube,
+  Instagram
 }
